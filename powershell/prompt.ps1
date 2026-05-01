27144bd5a747a14b8fea5121874e059931966c71
user3301 <26126682+user3301@users.noreply.github.com>
2026-05-01 10:35:21 +1000

    fix oh-my-posh statusline

--- powershell/prompt.ps1
+++ powershell/prompt.ps1
@@ -1,4 +1,4 @@
 # Initialise oh-my-posh with the jandedobbeleer theme
 if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
+    oh-my-posh init pwsh --config "$ProfileRoot\themes\jandedobbeleer.omp.json" | Invoke-Expression
 }
